6e2fa6cb9d56a350c4ce0a5faf0bc585ab2dd119
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-03-15 21:40:08 +0530

    name heading

--- components/Header.jsx
+++ components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
     return ()=>window.removeEventListener('scroll', scrollYPos);
   });
 
-  return <header className={`${header ? 'py-4 bg-white shadow-lg dark:bg-accent' : 'py-6 dark:bg-transparent'} sticky top-0 z-30 transition-all`}>
+  return <header className={`${header ? 'py-4 bg-white shadow-lg dark:bg-accent' : 'py-6 dark:bg-transparent'} sticky top-0 z-38 transition-all ${pathname === '/' && 'bg-[#fef9f5]'}`}>
     <div className="container mx-auto">
       <div className="flex justify-between items-center">
      <Logo />
