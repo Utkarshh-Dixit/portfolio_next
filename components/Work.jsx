49ebaee09e68993770e90cb603b2bb1de17fb6de
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-05-19 15:38:09 +0530

    added deployed link

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,52 +1,55 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { Button } from "./ui/button";
 
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
-import {Pagination} from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 
 import ProjectCard from "@/components/ProjectCard";
 
-
 const projectData = [
   {
-    image: '/work/ks_project.png',
-    category: 'MERN Stack',
-    name: 'Kaarigar-Sathi',
-    description: 'Your trusted platform connecting users with skilled local artisans seamlessly. Empowering communities with real-time listings, secure interactions, and personalized recommendations.',
-    link: '/',
-    github: '/'
+    image: "/work/ks_project.png",
+    category: "MERN Stack",
+    name: "Kaarigar-Sathi: Where skills meet needs",
+    description:
+      "Your trusted platform connecting users with skilled local artisans seamlessly. Empowering communities with real-time listings, secure interactions, and personalized recommendations.",
+    link: "https://kaarigarsathi-project.onrender.com",
+    github: "https://github.com/Utkarshh-Dixit/KaarigarSathi-Project",
   },
   {
-    image: '/work/insta_project.png',
-    category: 'MERN Stack',
-    name: 'Insta-Replica',
-    description: '"I have created a replica of Instagram, offering users a platform to share photos, connect, and engage with others through likes, comments, and follows.',
-    link: '/',
-    github: '/'
+    image: "/work/insta_project.png",
+    category: "MERN Stack",
+    name: "SnapWorld: Capture. Share. Explore",
+    description:
+      '"I have created a replica of Instagram, offering users a platform to share photos, connect, and engage with others through likes, comments, and follows.',
+    link: "/",
+    github: "https://github.com/Utkarshh-Dixit/instaclone",
   },
   {
-    image: '/work/3.png',
-    category: 'MERN Stack',
-    name: 'PinCraft: A visual discovery tool',
-    description: 'I have built a platform that enables users to discover, save, and share visual inspirations through personalized boards and collections.',
-    link: '/',
-    github: '/'
+    image: "/work/3.png",
+    category: "MERN Stack",
+    name: "PinCraft: A visual discovery tool",
+    description:
+      "I have built a platform that enables users to discover, save, and share visual inspirations through personalized boards and collections.",
+    link: "/",
+    github: "https://github.com/Utkarshh-Dixit/pinterest-clone",
   },
   {
-    image: '/work/amazon_project.png',
-    category: 'ReactJS, Firebase',
-    name: 'Amazon Clone',
-    description: 'I have crafted a replica of Amazon, complete with essential shopping features and payment processing, providing users with a familiar online shopping experience',
-    link: '/',
-    github: '/'
+    image: "/work/amazon_project.png",
+    category: "ReactJS, Firebase",
+    name: "MarketHub: Your one-stop shop",
+    description:
+      "I have crafted a replica of Amazon, complete with essential shopping features and payment processing, providing users with a familiar online shopping experience",
+    link: "https://clone-f1ed7.web.app/",
+    github: "https://github.com/Utkarshh-Dixit/amazon-clone",
   },
-]
+];
 
 const Work = () => {
   return (
@@ -54,26 +57,42 @@ const Work = () => {
       <div className="container mx-auto">
         <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
           <h2 className="section-title mb-4">Latest Projects</h2>
-          <p className="subtitle mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quae. Velit inventore perferendis nostrum dolorum vero eaque corrupti repellat dignissimos. Omnis labore nostrum deleniti, voluptatem vero ducimus. Impedit, dolorum adipisci!</p>
-          <Link href='/projects'><Button>All Projects</Button></Link>
+          <p className="subtitle mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quae.
+            Velit inventore perferendis nostrum dolorum vero eaque corrupti
+            repellat dignissimos. Omnis labore nostrum deleniti, voluptatem vero
+            ducimus. Impedit, dolorum adipisci!
+          </p>
+          <Link href="/projects">
+            <Button>All Projects</Button>
+          </Link>
         </div>
 
         <div className=" xl:max-w-[1000px] xl:absolute right-0 top-0">
-          <Swiper className="h-[480px]" slidesPerView={1} breakpoints={{
-            640: {
-              slidesPerView: 2,
-            }
-          }} spaceBetween={30} modules={[Pagination]} pagination={{clickable:true}}>
-            {projectData.slice(0.4).map((project, index)=>{
-              return <SwiperSlide key={index}>
-                <ProjectCard project={project}/>
-              </SwiperSlide>
+          <Swiper
+            className="h-[480px]"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {projectData.slice(0.4).map((project, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <ProjectCard project={project} />
+                </SwiperSlide>
+              );
             })}
           </Swiper>
         </div>
       </div>
     </section>
-  )
-}
-0
-export default Work
+  );
+};
+0;
+export default Work;
